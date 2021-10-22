9caf7a7bc3be2de1a2108371c791dd587a169535
vinicius rodrigues <vinitecnico@gmail.com>
2021-10-21 22:33:42 -0300

    feat: added DropdownButton

--- lib/screens/transfer/form.dart
+++ lib/screens/transfer/form.dart
@@ -14,13 +14,14 @@ class FormTransferState extends State<FormTransfer> {
   final TextEditingController _controladorCampoNumeroConta =
       TextEditingController();
   final TextEditingController _controllerAmount = TextEditingController();
+  String? _dropdownValue = 'QR-Code';
 
   void _createTransfer(context) {
     final int? accountNumber = int.tryParse(_controladorCampoNumeroConta.text);
     final double? amount = double.tryParse(_controllerAmount.text);
-    if (accountNumber != null && amount != null) {
-      final Transfer? transferCreated = Transfer(
-          amount, 'transferência via lista de contatos', accountNumber);
+    if (accountNumber != null && amount != null && _dropdownValue != null) {
+      final Transfer? transferCreated =
+          Transfer(amount, _dropdownValue.toString(), accountNumber);
       debugPrint("$transferCreated");
       Navigator.pop(context, transferCreated);
     }
@@ -33,6 +34,25 @@ class FormTransferState extends State<FormTransfer> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: DropdownButton(
+                value: _dropdownValue,
+                isExpanded: true,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _dropdownValue = newValue!;
+                  });
+                },
+                items: <String>['QR-Code', 'lista de contato']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
             Editor(
               controller: _controladorCampoNumeroConta,
               title: 'Account number',
